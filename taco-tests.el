7e2c121073a118112b1c07f092110c4892bbf1e2
Jim Porter <jporterbugs@gmail.com>
2023-01-01 18:33:33 -0800

    Update copyright year

--- taco-tests.el
+++ taco-tests.el
@@ -1,6 +1,6 @@
 ;;; taco-tests.el --- Tests for taco -*- lexical-binding: t -*-
 
-;; Copyright (C) 2021-2022 Jim Porter
+;; Copyright (C) 2021-2023 Jim Porter
 
 ;; Author: Jim Porter
 ;; Keywords: tests
